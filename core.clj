71c763da53b15619bc3ec475c3f5faf5445db803
Marcin Kruglik <marcin.kruglik@gmail.com>
2016-03-17 09:22:04 +0000

    Adds logic for changing rooms and refactoring

--- core.clj
+++ core.clj
@@ -1,14 +1,25 @@
 (ns fun-pro.core)
 
-(def descriptions { :1 '("you are in the living room")
-                    :2 '("you are in the hallway")
+(def descriptions { :1 '(you are in the living room)
+                    :2 '(you are in the hallway)
                    })
 
-(def directions { :1 {:north 2, :south 0, :east 0, :west 0}
-                  :2 {'north 0, 'south 1, 'east 0, 'west 0}})
-
-(defn get-room-description [id]
-  (get descriptions id))
+(def directions { :1 {:north :2, :south :0, :east :0, :west :0}
+                  :2 {:north :0, :south :1, :east :0, :west :0}})
 
 (defn lookup [room-id direction]
  (get (get directions room-id ) direction))
+
+(defn startgame [room-id]
+  (loop [rid room-id]
+    (println (clojure.string/join " "(get descriptions rid)))
+    (let [input (keyword (read-line))]
+      (cond
+       (= input :quit) "bye bye"
+       (some #(= input %) '(:north :south :east :west) )
+       (let [direction (lookup rid input)]
+         (if (= 0 direction)
+           (recur rid)
+           (recur direction)))
+       :else (recur input))      
+      ))) 

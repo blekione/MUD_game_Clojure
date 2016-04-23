0c9b38343760dcfa7b5b0b34a0b5a659e1bc4b60
Marcin Kruglik <marcin.kruglik@gmail.com>
2016-04-23 21:07:01 +0100

    Working on advanced command-line processor

--- core.clj
+++ core.clj
@@ -8,6 +8,8 @@
 (def directions { :1 {:north :2, :south :0, :east :0, :west :0}
                   :2 {:north :0, :south :1, :east :0, :west :0}})
 
+
+;; check which direction are available
 (defn lookup [room-id direction]
  (get (get directions room-id ) direction))
 
@@ -44,3 +46,90 @@
        (println reply)
        (when-not (= :quit cmd)
          (recur)))))
+
+;; advanced command-line processor
+
+(def responses
+  {:1 "What type of films do you like?"
+   :2 "So you like gore?"
+   :7 "Shall I recommend a gory film for you?"
+   :8 "Shall I recommend a non-gory scary film for you?"})
+
+(def decisiontable
+  {:1 {"comedy" :20
+       "very scary" :2
+       "thrillers" :6
+       "not animated" :5
+       "horror" :2
+       "scifi" :4}
+   :2 {"some" :8
+       "a lot" :7
+       "yes" :7
+       "no" :8
+       "not really" :8}
+   :7 {"yes" "gory"
+       "ok" "gory"
+       "no" :0}
+   :8 {"yes" "non-gory"
+       "ok" "gory"
+       "no" :0}})
+
+(defn get-response [id]
+  (get responses id))
+
+;; returns list of keywords from decisiontable "attached" to id 
+(defn get-keywords [id]
+  (let [keys (get decisiontable id)]
+    (map (fn [key] (first key)) keys)))
+
+;; outputs a list in the form: (0 0 0 2 0 0)
+(defn list-of-lengths [keylist tokens]
+  (map
+   (fn [x]
+     (let [set-value (conj #{} x)
+           tokens (into #{} tokens)
+           set (clojure.set/intersection tokens set-value)]
+       (* (/ (count set) (count set-value)) (count set)))
+     )
+  keylist))
+
+;; returns the index of element which match the pred
+(defn indices [pred coll]
+   (keep-indexed #(when (pred %2) %1) coll))
+
+;; returns index of the largest number from list
+(defn index-of-largest-number [list-of-numbers]
+  (let [n (last (sort list-of-numbers))]
+    (if (= 0 n)
+      false
+      (first (indices (fn [x] (= x n)) list-of-numbers)))))
+
+
+(defn lookup-tok [id tokens]
+  (let [record (get decisiontable id)
+        keylist (get-keywords id)
+        index (index-of-largest-number (list-of-lengths keylist tokens))]
+    (if (not= index false)
+      (second (nth record index))
+      false)))
+
+
+;; simplified in Clojure compared to Lisp solution translated into CLojure from above
+(defn lookup-clojure [id tokens]
+  (let [set-value (into #{} (get-keywords id))
+           tokens (into #{} tokens)
+           set (clojure.set/intersection tokens set-value)]
+    (if (> (count set) 1)
+      false 
+      (get (get decisiontable id) (first set)))))
+
+(defn recommend [initial-id]
+  (loop [id initial-id]
+    (println (format "%s\n> " (get-response id)))    
+    (let [input (read-line)
+          tokens (string/split input #" ")
+          response (lookup-clojure id tokens)]
+      (case response
+        false "You confused me. Can you be more specific"
+        :0 "bye bye"
+        (recur response))))) ;; default

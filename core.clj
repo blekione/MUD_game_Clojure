75c3454f592aa0d81c8dfd9ff09685171b2296c1
Marcin Kruglik <marcin.kruglik@gmail.com>
2016-04-24 19:39:11 +0100

    Adds function to get-directions from final-decision-table

--- core.clj
+++ core.clj
@@ -19,8 +19,8 @@
            "quit" :quit
            })
 
-(def decisiontable {:1 {:north :2, :west :3}
-                    :2 {:south :1}
+(def decisiontable {:1 {:north 1, :west 3}
+                    :2 {:south 1}
                     :3 {}})
 
 (def actions (merge look quit))
@@ -32,6 +32,29 @@
 ;; converting list of maps with single key-value pair into single map
 (defn list-to-map [input-lst]
   (zipmap (map #(first (first %)) input-lst) (map #(second (first %)) input-lst)))
+
+(def final-decision-table
+  (list-to-map merged-tables))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn check-if-direction [value]
+  (some #(= value %) '(:north :south :east :west)))
+
+(defn get-directions [id]
+  (let [record (get final-decision-table id)
+        result (filter #(check-if-direction (first %)) record)
+        n (count result)]
+    (case n
+      0 (println "You appear to have entered a room with no exits.")
+      1 (println (format "You can see an exit to the %s" (name (first (first result)))))
+      (let [lst-of-keywords (map first result)
+            lst-of-strings (map name lst-of-keywords)]
+        (println (str "You can see exits to the " (string/join " and " lst-of-strings)))))
+    ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 
 (def directions {:1 {:north :2, :south :0, :east :0, :west :0}

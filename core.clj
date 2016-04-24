0474814b58a009bf51813e08221f8dac17c52a7c
Marcin Kruglik <marcin.kruglik@gmail.com>
2016-04-24 18:30:57 +0100

    Creates decisiontable
    Each record in decisiontable is merged with actions

--- core.clj
+++ core.clj
@@ -7,6 +7,33 @@
                    :3 '(You are in a swamp)
                    })
 
+
+(def look {"directions" :look
+           "look" :look
+           "examine room" :look
+           })
+
+(def quit {"exit game" :quit
+           "quit game" :quit
+           "exit" :quit
+           "quit" :quit
+           })
+
+(def decisiontable {:1 {:north :2, :west :3}
+                    :2 {:south :1}
+                    :3 {}})
+
+(def actions (merge look quit))
+
+;; merge actions into every record of decisiontable - result is list of maps with single key-value pair
+(def merged-tables
+  (map #(merge (assoc {} (first %) (merge (second %) actions))) decisiontable))
+
+;; converting list of maps with single key-value pair into single map
+(defn list-to-map [input-lst]
+  (zipmap (map #(first (first %)) input-lst) (map #(second (first %)) input-lst)))
+
+
 (def directions {:1 {:north :2, :south :0, :east :0, :west :0}
                  :2 {:north :0, :south :1, :east :0, :west :0}})
 
